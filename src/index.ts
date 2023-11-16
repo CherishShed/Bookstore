fd7e380aa008c2a3275ca0fa548724a8b8c88fc6
CherishShed <c.shed2000@gmail.com>
2023-11-16 06:05:51 +0100

    wrote get and post requests

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
 import express from 'express'
-
+import 'dotenv/config'
+import { Book } from './model/database.model'
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.get('/api', (req, res) => {
-  res.status(200).json({ message: 'Done' })
+app.get('/api', async (req, res) => {
+  Book.find()
+    .then(result => {
+      res.json({ message: 'succesful', result })
+    })
+    .catch(err => {
+      res.json({ message: 'an error occurred', error: err })
+    })
 })
-app.listen(8081, () => {
-  console.log('listening on port 8081')
+app.get('/api/:id', async (req, res) => {
+  const { id } = req.params
+  Book.findById(id)
+    .then(result => {
+      res.json({ message: 'succesful', result })
+    })
+    .catch(err => {
+      res.json({ message: 'an error occurred', error: err })
+    })
+})
+app.post('/api/books', async (req, res) => {
+  const { name, number } = req.body
+  const newBook = new Book({ name, number })
+  newBook
+    .save()
+    .then(result => {
+      res.json({ message: 'succesful', result })
+    })
+    .catch(err => {
+      res.json({ message: 'an error occurred', error: err })
+    })
+})
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`)
 })
--- src/model/database.model.ts
+++ src/model/database.model.ts
@@ -1,3 +1,21 @@
-import mongoose from 'mongoose'
+import mongoose, { Document } from 'mongoose'
+import 'dotenv/config'
 
-const bookSchema = mongoose
+mongoose
+  .connect(process.env.CONNECTION_STRING!)
+  .then(() => {
+    console.log('Connection Succeeded')
+  })
+  .catch(err => {
+    console.log(`An error occurred ${err}`)
+  })
+export interface bookType extends Document {
+  name: string
+  number: number
+}
+const bookSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  number: { type: Number, required: true },
+})
+
+export const Book = mongoose.model<bookType>('Book', bookSchema)
